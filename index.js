e2c33fddd65218da0f5630dad705621e7dd57551
ehasanulh1 <ehasanulh1@gmail.com>
2022-12-21 09:45:28 +0600

    Post API 64-3

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -6,14 +7,26 @@ app.get('/', (req, res) => {
     res.send('Simple Node Server Running');
 });
 
+app.use(cors());
+app.use(express.json());
+
 const users = [
     { id: 1, name: 'Shabana', email: 'shabana@gamil.com' },
-    { id: 1, name: 'Shabnor', email: 'shabnor@gamil.com' },
-    { id: 1, name: 'Shahnaz', email: 'shahnaz@gamil.com' }
+    { id: 2, name: 'Shabnor', email: 'shabnor@gamil.com' },
+    { id: 3, name: 'Shahnaz', email: 'shahnaz@gamil.com' }
 ];
 
 app.get('/users', (req, res) => {
     res.send(users);
+});
+
+app.post('/users', (req, res) => {
+    console.log('Post API called');
+    const user = req.body;
+    user.id = users.length + 1;
+    users.push(user);
+    console.log(user)
+    res.send(user);
 })
 
 app.listen(port, () => {
